92dc5aef27e3e3fed51673d09260dc6a6f7f3e67
Mark Goddard <mark@stackhpc.com>
2020-02-28 15:17:39 +0000

    Allow limiting idle time only on matching lines

--- asciinema-editor.py
+++ asciinema-editor.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import json
+import re
 import sys
 
 
@@ -31,10 +32,12 @@ class Recording(object):
         for line in self.lines:
             line[0] = line[0] + offset
 
-    def limit_idle_time(self, idle_time_limit):
+    def limit_idle_time(self, idle_time_limit, match):
         last_time_old = 0
         last_time_new = 0
         for line in self.lines:
+            if match and not match.search(line[2]):
+                continue
             curr_time_old = line[0]
             diff = curr_time_old - last_time_old
             diff = min(diff, idle_time_limit)
@@ -59,14 +62,18 @@ def main():
         input_filename = sys.argv[2]
         output_filename = sys.argv[3]
         idle_time_limit = float(sys.argv[4])
+        if len(sys.argv) > 5:
+             match = re.compile(sys.argv[5])
+        else:
+             match = None
         r = Recording.from_file(input_filename)
-        r.limit_idle_time(idle_time_limit)
+        r.limit_idle_time(idle_time_limit, match)
         r.write(output_filename)
     elif op == "-h":
         print("asciinema video editor")
         print("Usage:")
         print("  {} add-offset <input file> <output file> <offset>".format(sys.argv[0]))
-        print("  {} limit-idle-time <input file> <output file> <idle time limit>".format(sys.argv[0]))
+        print("  {} limit-idle-time <input file> <output file> <idle time limit> [<regex>]".format(sys.argv[0]))
     else:
         raise Exception("Unexpected operation: {}".format(op))
 
